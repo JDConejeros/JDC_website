b4774770fbe997b80fc5763341fa5d0c4d5d8cd7
José Conejeros <jdconejeros@uc.cl>
2023-02-20 11:49:34 -0300

    CV y otros aspectos del sitio

---
+++ _site/script.js
@@ -0,0 +1,9 @@
+// Inicializar Typed.js
+var options = {
+  stringsElement: '#cd-words-wrapper',
+  typeSpeed: 60,
+  backSpeed: 30,
+  backDelay: 2000,
+  loop: true
+};
+var typed = new Typed('.cd-headline', options);
