06608c0188fc3c9ceca6c39f389d3ff5f5f1956f
José Conejeros <jdconejeros@uc.cl>
2023-02-20 14:20:30 -0300

    UP publish

--- _site/script.js
+++
@@ -1,9 +0,0 @@
-// Inicializar Typed.js
-var options = {
-  stringsElement: '#cd-words-wrapper',
-  typeSpeed: 60,
-  backSpeed: 30,
-  backDelay: 2000,
-  loop: true
-};
-var typed = new Typed('.cd-headline', options);
